c17d3b4af9640af672f6eb2403f29b68084b09fb
Parth Ranawat <parth.ranawat@gmail.com>
2023-03-06 00:27:12 -0500

    Adds site map coordinate

--- src/components/SiteMap/SiteMap.js
+++ src/components/SiteMap/SiteMap.js
@@ -1,68 +1,51 @@
 import React from "react";
 import SiteMapImage from "./Jefferson_Exercise_Plot_Stand_Map_Close_Aerial.jpg";
 import "./SiteMap.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const SiteMap = () => {
   return (
-    <div className="image">
-      <map name="workmap">
-        <area
-          shape="circle"
-          coords="550,100,14"
-          alt="Plot 1"
-          href="./Plot1.html"
-        />
-        <area
-          shape="circle"
-          coords="423,127,14"
-          alt="Plot 2"
-          href="./Plot2.html"
-        />
-        <area
-          shape="circle"
-          coords="432,208,14"
-          alt="Plot 3"
-          href="./Plot3.html"
-        />
-        <area
-          shape="circle"
-          coords="315,214,14"
-          alt="Plot 4"
-          href="./Plot4.html"
-        />
-        <area
-          shape="circle"
-          coords="320,282,14"
-          alt="Plot 5"
-          href="./Plot5.html"
-        />
-        <area
-          shape="circle"
-          coords="192,312,14"
-          alt="Plot 6"
-          href="./Plot6.html"
-        />
-        <area
-          shape="circle"
-          coords="110,405,14"
-          alt="Plot 7"
-          href="./Plot7.html"
-        />
-        <area
-          shape="circle"
-          coords="212,381,14"
-          alt="Plot 8"
-          href="./Plot8.html"
-        />
-      </map>
-      <img
-        src={SiteMapImage}
-        width="100%"
-        heigth="100%"
-        useMap="#workmap"
-        alt="Site Map"
-      ></img>
-    </div>
+    <Router>
+      <div className="image">
+        <Link to={"./Plot1"}>
+          <button className="point1 points"></button>
+        </Link>
+        <Link to={"./Plot1"}>
+          <button className="point2 points"></button>
+        </Link>
+
+        <Link to={"./Plot1"}>
+          <button className="point3 points"></button>
+        </Link>
+
+        <Link to={"./Plot1"}>
+          <button className="point4 points"></button>
+        </Link>
+        <Link to={"./Plot1"}>
+          <button className="point5 points"></button>
+        </Link>
+
+        <Link to={"./Plot1"}>
+          <button className="point6 points"></button>
+        </Link>
+
+        <Link to={"./Plot1"}>
+          <button className="point7 points"></button>
+        </Link>
+
+        <Link to={"./Plot1"}>
+          <button className="point8 points"></button>
+        </Link>
+
+        <img
+          src={SiteMapImage}
+          width="100%"
+          heigth="100%"
+          useMap="#workmap"
+          alt="Site Map"
+        ></img>
+      </div>
+    </Router>
   );
 };
 
